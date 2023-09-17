3d2f671c4601e3284a19eda04cf2a985ea139282
rafikulislam775 <islamrafikul.jp@gmail.com>
2023-09-18 05:54:38 +0900

    added new files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/Nav/Nvbar/Navbar";
+// import Nav from "./components/Nav/Nav";
+
+function App() {
+  return (
+    <>
+      {/* <Nav></Nav> */}
+      <Navbar></Navbar>
+    </>
+  );
+}
+
+export default App;
